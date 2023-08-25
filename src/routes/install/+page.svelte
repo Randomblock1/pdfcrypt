caf7bfde6427969e7a73dfeda292e28ea4f5d4a4
Benjamin G <randomblock1@pm.me>
2023-08-25 16:32:42 -0500

    Add Store badge

--- src/routes/install/+page.svelte
+++ src/routes/install/+page.svelte
@@ -1,10 +1,11 @@
+<script type="module" src="https://get.microsoft.com/badge/ms-store-badge.bundled.js"></script>
 <div class="m-8">
-    <h1 class="text-3xl rounded-lg p-2.5 bg-primary text-white mb-4 w-fit">Installation</h1>
+    <h1 class="text-3xl rounded-lg p-2.5 bg-primary text-white my-4 w-fit">Installation</h1>
     <p>
         This app is a progressive web app, which means it can be installed on your device like a
         native app. To install it, follow the instructions for your device below.
     </p>
-    <h2 class="text-2xl rounded-lg p-2.5 bg-primary text-white mb-4 w-fit">Android</h2>
+    <h2 class="text-2xl rounded-lg p-2.5 bg-primary text-white my-4 w-fit">Android</h2>
     <p>
         Chrome-based Android browsers have a little drawer appear at the bottom of the screen when
         you visit the app. Drag it upwards to show screenshots & the install button. Otherwise,
@@ -16,22 +17,24 @@
         <li>- Tap "Install App".</li>
         <li>- Tap "Install".</li>
     </ol>
-    <h2 class="text-2xl rounded-lg p-2.5 bg-primary text-white mb-4 w-fit">iOS</h2>
+    <h2 class="text-2xl rounded-lg p-2.5 bg-primary text-white my-4 w-fit">iOS</h2>
     <ol>
         <li>- Open the app in Safari.</li>
         <li>- Tap the share button.</li>
         <li>- Tap "Add to Home Screen".</li>
         <li>- Tap "Add".</li>
     </ol>
-    <h2 class="text-2xl rounded-lg p-2.5 bg-primary text-white mb-4 w-fit">Windows</h2>
+    <h2 class="text-2xl rounded-lg p-2.5 bg-primary text-white my-4 w-fit">Windows</h2>
     <p>
-        Good news! This app is available in the Windows Store. You can install it from there, or by
-        clicking this link: <a
-            class="link"
-            href="https://www.microsoft.com/store/apps/9PCHG45LJXZN">
-            Install from the Windows Store</a
-        >.
+        Good news! This app is available in the Microsoft Store.
     </p>
+    <ms-store-badge
+	    productid="9PCHG45LJXZN"
+	    cid="installtab"
+	    theme="auto"
+	    language="en-US"
+	    animation="on" 
+    />
     <ol>
         <li>- Open the app in Edge.</li>
         <li>- Click the three dots in the top right corner.</li>
